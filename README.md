756f06eedbcdf65dceccbf1a10c201c1294184ed
dci-student <maxxik007@gmail.com>
2023-01-20 14:22:42 +0100

    Deleted Unnecessary code and text.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple calculator task
+
+Simple program in Python with `input()` function!
--- main.py
+++ main.py
@@ -1,8 +1,1 @@
 # VERY SIMPLE CALCULATOR
-
-print(' WELCOME TO CALCULATOR '.center(50, '='))
-
-first_choise = input('Would you like to start? y/n')
-
-while first_choise != 'n':
-    first_choise = input('Would you like to start? y/n')

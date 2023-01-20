aced5934525851cf6bdc0c7ed2f9fb00e6e5a1be
dci-student <maxxik007@gmail.com>
2023-01-20 14:31:40 +0100

    Added jokes list and random choise

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+from random import choice
+
 # VERY SIMPLE CALCULATOR
 
 print(' WELCOME TO CALCULATOR '.center(50, '='))
 
-first_choise = 'yes'
+first_choise = 'y'
 
-while first_choise == 'yes':
+while first_choise == 'y':
     
     action = int(input('What would you like to do?\n\n•[1] Add\n•[2] Multiply\n•[3] Divide\n•[4] Subtract\n•[5] Random joke\n\n'))
 
@@ -21,8 +23,8 @@ while first_choise == 'yes':
         print()
         print('Result: ', int(input('Enter first number: ')) - int(input('Enter second number: ')))
     elif action == 5:
-        pass
-
+        jokes = ['How many programmers does it take to change a light bulb?\nNone – It’s a hardware problem', 'A programmer walks to the butcher shop and buys a kilo of meat.\nAn hour later he comes back upset that the butcher shortchanged him by 24 grams.', 'There are three kinds of lies: Lies, damned lies, and benchmarks.', 'Debugging: Removing the needles from the haystack.']
+        print(choice(jokes))
     print()
     first_choise = input('Would you like to use calculator further? y/n ')
     print()
